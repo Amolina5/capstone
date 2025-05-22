dea7de63efb1cc1753928d473e8df5845e068dce
Alex <amolina555@hotmail.com>
2025-05-21 20:05:17 -0500

    Switch to PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,7 @@ Django settings for config project.
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,12 +69,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Database - Use SQLite for everything (simplest solution)
+# Database - Use PostgreSQL on Heroku, SQLite locally
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
